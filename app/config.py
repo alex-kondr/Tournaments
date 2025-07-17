25f54507a3882e93e2a3b6e73a09fd27c09a5bdd
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-07-17 20:38:33 +0300

    save

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlalchemy_uri: str = "mysql+aiomysql://localhost:5432/tornaments"
+    sqlalchemy_uri: str = "mysql+aiomysql://avnadmin:AVNS_3-yt9ee4lQSFabtFpzi@mysql-31182646-alex-kondr.j.aivencloud.com:13316/tornaments"
     secret_key: str = "super secret key"
     exp_time_minutes: int = 30
 
