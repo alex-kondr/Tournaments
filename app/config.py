a04b35c9dc25166161f16cda1a7736163383ce56
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-07-22 21:38:34 +0300

    save

--- app/config.py
+++ app/config.py
@@ -1,10 +1,12 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     sqlalchemy_uri: str = "mysql+aiomysql://localhost:5432/tornaments"
     secret_key: str = "super secret key"
     exp_time_minutes: int = 30
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
